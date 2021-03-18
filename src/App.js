22be3230d5df48ec5b12e32c7aacab7289dcbdaf
unknown <rodrigo.toblli@gmail.com>
2021-03-18 19:21:44 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Pentagono from './components/Pentagono'
+
+function App() {
+  return (
+    <div className="App">
+      <section className='main'>
+
+      <nav className='navBar'>
+            <h1 className='title'>MOREKO</h1>
+      </nav>
+
+      <Pentagono/>
+
+
+
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
