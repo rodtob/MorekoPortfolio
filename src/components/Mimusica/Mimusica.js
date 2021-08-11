2efe76b95429c6ba475146f3e6f8e7fb03d2121f
Moreko <morenojuanjose99@gmail.com>
2021-08-10 23:20:35 -0300

    agregue traducciones y detalles de contenido

--- src/components/Mimusica/Mimusica.js
+++ src/components/Mimusica/Mimusica.js
@@ -5,17 +5,23 @@ import "./Mimusica.scss";
 const Mimusica = () => {
   const theworks = [
     {
-      title: "Original SQ",
-      year: "2021",
+      title: "Esperandote",
+      year: "2019",
       describe: "Un gran disco",
       estilo: "Pop",
       site: "https://open.spotify.com/embed/track/1CEEsUrcplfYapCR8FRtW8",
     },
     {
       title: "Anestesiados",
-      year: "2020",
+      year: "2019",
       describe: "boxr",
       site: "https://open.spotify.com/embed/track/0LdQ1Mu3V4qx96xxMGfdmN",
+    },
+    {
+      title: "Extraña",
+      year: "2021",
+      describe: "boxr",
+      site: "https://open.spotify.com/embed/album/0cHNwjuI4B8WYfO4uzVFK5",
     },
     {
       title: "Glosa",
@@ -23,18 +29,6 @@ const Mimusica = () => {
       describe: "boxr",
       site: "https://open.spotify.com/embed/track/7C1BA9hv7eD105mPaCHQpF",
     },
-    {
-      title: "Anestesiados",
-      year: "2020",
-      describe: "boxr",
-      site: "https://open.spotify.com/embed/track/0LdQ1Mu3V4qx96xxMGfdmN",
-    },
-    {
-      title: "Glosa",
-      year: "2020",
-      describe: "boxr",
-      site: "https://open.spotify.com/embed/track/7C1BA9hv7eD105mPaCHQpF",
-    }
   ];
 
   return (
